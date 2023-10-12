a5f84339c9e35bd0cc0d4354d05c8a906c5425db
FabrizioEliasTracuzzi <fabritracuzzi@gmail.com>
2023-10-12 20:58:43 -0300

    Ultimo cambio 12/10

---
+++ Javascript/app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Encuentra los elementos con la clase "text-image"
+    var textImages = document.querySelectorAll(".text-image");
+    
+    // Agrega la clase "show" a cada elemento con un retraso
+    textImages.forEach(function(textImage, index) {
+        setTimeout(function() {
+            textImage.classList.add("show");
+        }, index * 1000); // Cambia el valor de 1000 según la duración de tu animación
+    });
+});
